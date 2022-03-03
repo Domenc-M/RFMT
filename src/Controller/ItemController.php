6be3986a026b8859690cc28460bb3d64466570b7
Maxime DOMENC <maxime.domenc@gmail.com>
2022-03-03 10:59:28 +0100

    Upload d'image fonctionnelle avec edit et delete

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -30,7 +30,7 @@ class ItemController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $img = $form->get('img')->getData();
+            $img = $form->get('imgFile')->getData();
             if(!$img)
             {
                 $item->setImg("basic.png");
@@ -78,15 +78,48 @@ class ItemController extends AbstractController
     /**
      * @Route("/{id}/edit", name="item_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Item $item, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Item $item, EntityManagerInterface $entityManager,
+                        SluggerInterface $slugger): Response
     {
         $form = $this->createForm(ItemType::class, $item);
+        $oldImg = $item->getImg();
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $img = $form->get('imgFile')->getData();
+
+            if(!$img || $img->getClientOriginalName() == $oldImg)
+            {
+                $item->setImg($oldImg);
+            }
+            else
+            {
+                $originalFilename = pathinfo($img->getClientOriginalName(), PATHINFO_FILENAME);
+                //Slug to make file name safe
+                $safeFilename = $slugger->slug($originalFilename);
+                //Uniqid to prevent twice the same name in DB, so they dont replace each other.
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$img->guessExtension();
+                try {
+                    $img->move(
+                        $this->getParameter('img_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // TODO
+                }
+
+                if($oldImg != "basic.png")
+                {
+                    unlink($this->getParameter('img_directory')."/".$oldImg);
+                }
+
+                $item->setImg($newFilename);
+            }
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('item_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_searchOwn', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('item/edit.html.twig', [
@@ -101,10 +134,14 @@ class ItemController extends AbstractController
     public function delete(Request $request, Item $item, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$item->getId(), $request->request->get('_token'))) {
+            if($item->getImg() != "basic.png")
+            {
+                unlink($this->getParameter('img_directory')."/".$item->getImg());
+            }
             $entityManager->remove($item);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('item_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_searchOwn', [], Response::HTTP_SEE_OTHER);
     }
 }
